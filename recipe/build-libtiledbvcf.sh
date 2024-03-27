c2af8b3a9d03f65b58fd0bc7ad0a13a492990d02
Adam Wenocur <awenocur@gmail.com>
2024-03-27 18:06:05 -0400

    remove problematic linker arg

--- recipe/build-libtiledbvcf.sh
+++ recipe/build-libtiledbvcf.sh
@@ -4,6 +4,10 @@ set -exo pipefail
 
 mkdir libtiledbvcf-build && cd libtiledbvcf-build
 
+# wrap CXX compiler to remove problematic args
+export NN_CXX_ORIG=$CXX
+export CXX="${RECIPE_DIR}/cxx_wrap.sh"
+
 cmake \
   -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
   -DOVERRIDE_INSTALL_PREFIX=OFF \
