f8c441d77f8f9d62e40a820adc322a5b4b827ee1
Dušan Baran <me.dusanbaran@gmail.com>
2024-12-04 13:23:32 +0100

    Test out vcpkg VCF rework

--- recipe/build-libtiledbvcf.sh
+++ recipe/build-libtiledbvcf.sh
@@ -8,16 +8,17 @@ mkdir libtiledbvcf-build && cd libtiledbvcf-build
 export NN_CXX_ORIG=$CXX
 export CXX="${RECIPE_DIR}/cxx_wrap.sh"
 
-cmake \
-  -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
-  -DOVERRIDE_INSTALL_PREFIX=OFF \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DFORCE_EXTERNAL_HTSLIB=OFF \
-  ../libtiledbvcf
+cmake -S libtiledbvcf -B build \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D BUILD_SHARED_LIBS=ON \
+  -D TILEDBVCF_ENABLE_TESTS=OFF \
+  -D TILEDBVCF_FORCE_EXTERNAL_DEPENDENCIES=ON \
+  -D TILEDBVCF_INSTALL_TILEDB=OFF \
+  -D CMAKE_PREFIX_PATH=$CONDA_PREFIX
 
-make -j ${CPU_COUNT}
+cmake --build build --config Release
 
-make install-libtiledbvcf
+cmake --install build --config Release
 
 # Cleanup
 cd ../ && rm -r libtiledbvcf-build
